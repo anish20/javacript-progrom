0a7f8de185e16b6847354081546655e59f7dcd47
Dn Vidya <anish@ferventsoft.com>
2020-01-04 19:57:06 +0530

    Add new updated

--- js/script3.js
+++ js/script3.js
@@ -1,17 +1,16 @@
-// create table 1-20
+// create table random row and column
 'use strict'
 let rows=prompt("Please enter the rows");
 let cols=prompt("Please enter the columns");
 debugger
-if(rows=="" || rows==null){
+if(rows=="" || rows==null || rows==0){
     rows=10;
 }
-if(cols=="" || cols==null){
+if(cols=="" || cols==null || cols==0){
     cols=10;
 }
 createTable(rows,cols);
 function createTable(r,c){
-    
 let i=1,j=1;
 let output="<table>";
 for(i=1;i<=r;i++){
